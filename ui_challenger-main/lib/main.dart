9a148252c0e374849a0ac2ea4df5af02773970d6
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-10-23 16:17:24 -0300

    constantes

--- ui_challenger-main/lib/main.dart
+++ ui_challenger-main/lib/main.dart
@@ -1,37 +1,19 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ui_challenger/pages/onboarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Plants",
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
+    return const MaterialApp(
+      title: 'Onboarding Screen',
+      home: OnboardingScreen(),
       debugShowCheckedModeBanner: false,
-      home: MainPage(),
     );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
